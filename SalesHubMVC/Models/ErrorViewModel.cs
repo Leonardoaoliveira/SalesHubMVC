72f8620f5c44f38154a050c1ed146d7640267c65
Leonardo-arau <leo-a-oliveira@outlook.com>
2025-07-08 14:59:14 -0300

    Project created

---
+++ SalesHubMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesHubMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
